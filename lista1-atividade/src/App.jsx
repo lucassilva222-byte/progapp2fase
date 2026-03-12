c6f28eaa94974ade77be435a940b7d8a91b1f0ca
lucassilva222-byte <lucas_silva222@estudante.sesisenai.org.br>
2026-03-11 22:10:21 -0300

    atividade pneu

---
+++ lista1-atividade/src/App.jsx
@@ -0,0 +1,51 @@
+{
+//Calibrar os pneus do carro deve ser uma tarefa cotidiana de todos os motoristas. Para isto, os postos de gasolina possuem uma bomba de ar. A maioria das bombas atuais são eletrônicas, permitindo que o motorista indique a pressão desejada num teclado. Ao ser ligada ao pneu, a bomba primeiro lê a pressão atual e calcula a diferença de pressão entre a desejada e a lida. Com esta diferença ela esvazia ou enche o pneu para chegar na pressão correta.
+
+//Sua ajuda foi requisitada para desenvolver o programa da próxima bomba da SBC - Sistemas de Bombas Computadorizadas.
+
+//Escreva um programa que, dada a pressão desejada digitada pelo motorista e a pressão do pneu lida pela bomba, indica a diferença entre a pressão desejada e a pressão lida.  
+}
+
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [inputPressaoAtual, setInputPressaoAtual] = useState(0)
+const [inputPressaoEscolhida, setInputPressaoEscolhida] = useState(0)
+
+const[pressaoFaltante, setpressaoFaltante] = useState('')
+
+  function calcularPressao(){ 
+    setpressaoFaltante(inputPressaoEscolhida - inputPressaoAtual)
+
+  }
+
+  return (
+    <>
+      <h1>PRESSÃO DE PNEU</h1>
+      PRESSÃO ATUAL <input type= {Number}
+      value={inputPressaoAtual}
+      onChange={(e) => setInputPressaoAtual(e.target.value)}
+      />
+
+     <h2></h2>
+     PRESSÃO ESCOLHIDA <input type= {Number}
+      value={inputPressaoEscolhida}
+      onChange={(e) => setInputPressaoEscolhida(e.target.value)}
+      />
+
+      <h3></h3>
+
+      pressão faltante : {pressaoFaltante}
+
+      <div></div>
+
+      <button onClick={calcularPressao}>calcular</button>
+
+
+    </>
+    
+  )
+}
+
+export default App
